37e448ccb0165b1a6283ab9e092a760a90b97dae
JY-Xie <85422896+JY-Xie@users.noreply.github.com>
2023-12-20 00:05:52 +0800

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('templates/index1.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
